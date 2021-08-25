99d8d27671055f06d3ad909a695a76383b223cee
JasonPark <cjpark137@naver.com>
2021-08-25 11:15:21 +0900

    fix data

--- run.sh
+++
@@ -1,5 +0,0 @@
-python train.py -m 'RES50_Basic' -s 1 -b 128 -n 2 -e 200 -f 1 -g 5
-python train.py -m 'RES50_Basic' -s 2 -b 128 -n 2 -e 200 -f 1 -g 5
-python train.py -m 'RES50_Basic' -s 3 -b 128 -n 2 -e 200 -f 1 -g 5
-python train.py -m 'RES50_Basic' -s 4 -b 128 -n 2 -e 200 -f 1 -g 5
-python train.py -m 'RES50_Basic' -s 5 -b 128 -n 2 -e 200 -f 1 -g 5
